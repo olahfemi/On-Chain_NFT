6cf877bd9b6f96cad35b970e54becba29ad56355
Femi Olah <olahfemi@gmail.com>
2022-10-14 00:03:50 +0100

    Folder prepared

---
+++ deploy2.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+
+  const OlahFemi2 = await ethers.getContractFactory("OlahFemi2");
+  const olahfemi2 = await OlahFemi2.deploy();
+
+  await olahfemi2.deployed();
+
+  console.log(`OlahFemi2 is deployed to ${olahfemi2.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+// CONTRACTADDRESS on Goerli = 
